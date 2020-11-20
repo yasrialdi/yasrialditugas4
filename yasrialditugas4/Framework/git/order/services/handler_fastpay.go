45ddcea305e0852f8ffd2b820de4342795eedd4e
yasrialdi <72615276+yasrialdi@users.noreply.github.com>
2020-11-20 23:29:27 +0700

    Add files via upload

---
+++ yasrialditugas4/Framework/git/order/services/handler_fastpay.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"context"
+
+	cm "yasrialditugas4/Framework/git/order/common"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func (PaymentService) FastPayHandler(ctx context.Context, req cm.FastPayRequest) (res cm.FastPayResponse) {
+
+	return
+}
